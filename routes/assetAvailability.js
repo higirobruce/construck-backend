9371dd69e250ac75c10755b877d8d2b459261262
Bruce <higirobru@gmail.com>
2022-07-12 11:14:05 +0200

    Update assetAvailability.js

--- routes/assetAvailability.js
+++ routes/assetAvailability.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const assetAvblty = require("../models/assetAvailability");
 const findError = require("../utils/errorCodes");
 const _ = require("lodash");
+const moment = require("moment");
 
 router.get("/", async (req, res) => {
   try {
@@ -15,7 +16,7 @@ router.get("/", async (req, res) => {
 router.post("/getAnalytics", async (req, res) => {
   let { startDate, endDate } = req.body;
   try {
-    const avblties = await assetAvblty.model.find({
+    let avblties = await assetAvblty.model.find({
       date: { $gte: startDate, $lte: endDate },
     });
     let totAssets = 0;
@@ -23,18 +24,32 @@ router.post("/getAnalytics", async (req, res) => {
     let totUnavailable = 0;
     let totDispatched = 0;
     let totStandby = 0;
-    avblties.forEach((a) => {
-      totAssets =
-        totAssets + a.available
-          ? a.available
-          : 0 + a.unavailable
-          ? a.unavailable
-          : 0;
-      totAvailable = totAvailable + a.available ? a.available : 0;
-      totUnavailable = totUnavailable + a.unavailable ? a.unavailable : 0;
-      totDispatched = totDispatched + a.dispatched ? a.dispatched : 0;
-      totStandby = totStandby + a.standby ? a.standby : 0;
-    });
+    if (avblties.length > 0) {
+      avblties.forEach((a) => {
+        totAssets =
+          totAssets + a.available
+            ? a.available
+            : 0 + a.unavailable
+            ? a.unavailable
+            : 0;
+        totAvailable = totAvailable + a.available ? a.available : 0;
+        totUnavailable = totUnavailable + a.unavailable ? a.unavailable : 0;
+        totDispatched = totDispatched + a.dispatched ? a.dispatched : 0;
+        totStandby = totStandby + a.standby ? a.standby : 0;
+      });
+    } else {
+      avblties = await assetAvblty.model.find({});
+
+      avblties.forEach((a) => {
+        totAssets =
+          totAssets +
+          (a.available ? a.available : 0 + a.unavailable ? a.unavailable : 0);
+        totAvailable = totAvailable + (a.available ? a.available : 0);
+        totUnavailable = totUnavailable + (a.unavailable ? a.unavailable : 0);
+        totDispatched = totDispatched + (a.dispatched ? a.dispatched : 0);
+        totStandby = totStandby + (a.standby ? a.standby : 0);
+      });
+    }
 
     if (totAssets === 0) totAssets = 1;
     if (totAvailable === 0) totAvailable = 1;
