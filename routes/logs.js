5545010248354a7eee5574a8ff3bcf8e142a71ce
higirobruce <110108804+construck@users.noreply.github.com>
2022-08-31 11:37:35 +0200

    Update logs.js

--- routes/logs.js
+++ routes/logs.js
@@ -7,9 +7,27 @@ const _ = require("lodash");
 router.get("/", async (req, res) => {
   try {
     let logs = await (
-      await logData.model.find().populate("doneBy")
+      await logData.model
+        .find({
+          "payload.equipment.plateNumber": "RAB 791 L",
+          // action: "DISPATCH STOPPED",
+        })
+        .populate("doneBy")
     ).filter((l) => l.doneBy !== null);
-    res.status(200).send(logs);
+
+    let newObjs = logs.map((l, i) => {
+      return {
+        action: l.action,
+        id: l._id,
+        doneby: l.doneBy?.firstName + " " + l.doneBy?.lastName,
+        project: l.payload?.project?.prjDescription,
+        platenumber: l.payload?.equipment?.platenumber,
+        createdOn: l.createdOn,
+        duration: l.payload?.duration / (1000 * 60 * 60) + "hours",
+        totalRevenue: l.payload?.totalRevenue,
+      };
+    });
+    res.status(200).send(newObjs);
   } catch (err) {
     res.send(err);
   }
