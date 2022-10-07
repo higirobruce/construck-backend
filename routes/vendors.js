0c9b4c55d67f4e5eddc137ef0227b062f675a569
higirobruce <higirobru@gmail.com>
2022-10-07 09:46:43 +0200

    Hash Vendor password on creation

--- routes/vendors.js
+++ routes/vendors.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const venData = require("../models/vendors");
 const findError = require("../utils/errorCodes");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 router.get("/", async (req, res) => {
   try {
@@ -15,8 +16,9 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     let vendorToCreate = new venData.model(req.body);
+    let hashedPassword = await bcrypt.hash(req.body.password, 10);
+    vendorToCreate.password = hashedPassword;
     let vendorCreated = await vendorToCreate.save();
-
     res.status(201).send(vendorCreated);
   } catch (err) {
     let error = findError(err.code);
